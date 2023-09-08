0fbb4462e814b6b82d2be8ce6875e7a46358b2de
DhivyaSF4094 <dhivya.sf4094@gmail.com>
2023-09-08 13:09:11 +0530

    Unbound mode Sample

---
+++ TreeViewMaui/Platforms/Windows/App.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Windows.Graphics;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace GettingStarted.WinUI;
+
+/// <summary>
+/// Provides application-specific behavior to supplement the default Application class.
+/// </summary>
+public partial class App : MauiWinUIApplication
+{
+	/// <summary>
+	/// Initializes the singleton application object.  This is the first line of authored code
+	/// executed, and as such is the logical equivalent of main() or WinMain().
+	/// </summary>
+	public App()
+	{
+        const int WindowWidth = 600;
+
+        const int WindowHeight = 800;
+        this.InitializeComponent();
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+
+        {
+
+#if WINDOWS
+
+            var mauiWindow = handler.VirtualView;
+
+            var nativeWindow = handler.PlatformView;
+
+            nativeWindow.Activate();
+
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+
+            Microsoft.UI.WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+
+            AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+
+            appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+
+            nativeWindow.ExtendsContentIntoTitleBar = false;
+
+#endif
+
+        });
+    }
+
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
